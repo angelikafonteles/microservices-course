72d58d58bd8fde102398f2547ba2f228411a7d3b
Angelika Fonteles <angelikatf@gmail.com>
2020-11-10 16:03:55 -0300

    Ribbon load balance

--- hr-payroll/src/main/java/com/angelikafonteles/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/angelikafonteles/hrpayroll/HrPayrollApplication.java
@@ -1,9 +1,11 @@
-package com.angelikafonteles.hrpayroll;
+	package com.angelikafonteles.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name="hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
